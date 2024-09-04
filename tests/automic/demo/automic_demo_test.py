466688236db9df013858954972265e84dafbfc77
indraneeldongaonkar <indraneel.dongaonkar@astronomer.io>
2024-09-04 18:25:57 +0530

    Added tests

---
+++ tests/automic/demo/automic_demo_test.py
@@ -0,0 +1,22 @@
+from orbiter_translations.automic.xml_demo import translation_ruleset
+
+
+def test_ssis_demo(project_root):
+    actual = translation_ruleset.translate_fn(
+        translation_ruleset, (project_root / "tests_copy/automic/demo/workflow/")
+    )
+
+    assert list(actual.dags.keys()) == ["jobp.dummy.workflow"]
+    assert sorted(list(list(actual.dags.values())[0].tasks.keys())) == sorted(
+        [
+            "start",
+            "generate_file",
+            "process_and_upload_file",
+            "insert_into_database",
+            "end",
+        ]
+    )
+
+    assert sorted(
+        list(list(actual.dags.values())[0].tasks["insert_into_database"].downstream)
+    ) == sorted(["end"])
