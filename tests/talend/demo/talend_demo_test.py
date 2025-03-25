38f24be394e07f6c6f5d18198538884db5b38646
akshaykumarsalunke <akshaykumar.salunke@astronomer.io>
2025-03-25 17:31:04 +0530

    add translation for initial findings

--- tests/talend/demo/talend_demo_test.py
+++ tests/talend/demo/talend_demo_test.py
@@ -1,16 +1,18 @@
-from orbiter_translations.talend.json_demo import translation_ruleset
+from orbiter_translations.talend.talend_demo import translation_ruleset
 
 
 def test_talend_demo(project_root):
     actual = translation_ruleset.translate_fn(
         translation_ruleset, (project_root / "tests/talend/demo/workflow/")
     )
-    assert list(actual.dags.keys()) == ["..."]
+    assert list(actual.dags.keys()) == ["pipeline_2"]
     assert sorted(list(list(actual.dags.values())[0].tasks.keys())) == sorted(
         [
-            "...",
+            "airlines",
+            "foo",
+            "python_3_1"
         ]
     )
     assert sorted(
-        list(list(actual.dags.values())[0].tasks["..."].downstream)
-    ) == sorted(["..."])
+        list(list(actual.dags.values())[0].tasks["foo"].downstream)
+    ) == sorted(["python_3_1"])
