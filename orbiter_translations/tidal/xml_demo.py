0a69122d9044065c79a33990b5ee2a79f15e2a2d
fritz-astronomer <80706212+fritz-astronomer@users.noreply.github.com>
2025-10-27 15:26:06 -0400

    Tidal Demo File Fix (#45)
    * fix: tidal update w/ __file & __input_dir
    
    * chore: version bump
    
    * docs: standardize translation module docstring
    
    * cicd: test from py3.10->3.13
    
    * fix: most recent orbiter for tidal tests

--- orbiter_translations/tidal/xml_demo.py
+++ orbiter_translations/tidal/xml_demo.py
@@ -1,17 +1,22 @@
+"""Demo translation ruleset for Tidal XML files to Airflow DAGs
+
+Contact Astronomer @ https://astronomer.io/contact for access to our full translation.
+"""
 from __future__ import annotations
+
+from pathlib import Path
 
 from orbiter import clean_value
 from orbiter.file_types import FileTypeXML, xmltodict_parse
 from orbiter.objects.dag import OrbiterDAG
 from orbiter.objects.operators.bash import OrbiterBashOperator
-from orbiter.objects.task import OrbiterOperator
-from orbiter.objects.task_group import OrbiterTaskGroup
 from orbiter.rules import (
     dag_filter_rule,
     dag_rule,
     task_filter_rule,
     task_rule,
     task_dependency_rule,
+    create_cannot_map_rule_with_task_id_fn
 )
 from orbiter.rules.rulesets import (
     DAGFilterRuleset,
@@ -26,23 +31,32 @@ from orbiter.rules.rulesets import (
 
 
 @dag_filter_rule
-def basic_dag_filter(val: dict) -> list | None:
+def job_group_dag_filter(val: dict) -> list | None:
     """Filter input down to a list of dictionaries that can be processed by the `@dag_rules`"""
     return val.get("tes:jobgroup")
 
 
 @dag_rule
-def basic_dag_rule(val: dict) -> OrbiterDAG | None:
-    """Translate input into an `OrbiterDAG`"""
+def job_group_dag_rule(val: dict) -> OrbiterDAG | None:
+    """Translate `tes:jobgroup` into an `OrbiterDAG`
+
+    ```pycon
+    >>> job_group_dag_rule({"tes:name": "foo"})
+    from airflow import DAG
+    with DAG(dag_id='foo'):
+
+    ```
+    """
     dag_id = val.get("tes:name")
     return OrbiterDAG(dag_id=dag_id, file_path=f"{dag_id}.py")
 
 
 @task_filter_rule
 def basic_task_filter(val: dict) -> list | None:
-    """Filter input down to a list of dictionaries that can be processed by the `@task_rules`"""
     tasks = []
-    basedir = val.get("__file").parent.resolve()
+    __file: Path = val.get("__file")
+    __input_dir: Path = val.get("__input_dir")
+    basedir: Path = (__input_dir / __file.relative_to(__file.parts[0])).parent
     for path in basedir.rglob("*"):
         if path.name == "Job.xml":
             with path.open() as f:
@@ -50,20 +64,36 @@ def basic_task_filter(val: dict) -> list | None:
                 tasks.append(task["tes:job"][0])
     return tasks
 
+def task_common_args(val: dict) -> dict:
+    """Common properties for all tasks
+    - `tes:name` -> `task_id`
+    """
+    return {"task_id": val.get("tes:name", "UNKNOWN")}
+
+
 
 @task_rule(priority=2)
-def basic_ssh_task(val: dict) -> OrbiterOperator | OrbiterTaskGroup | None:
-    """Translate input into an Operator (e.g. `OrbiterBashOperator`). will be applied first, with a higher priority"""
-    task_id = val.get("tes:name")
-    if task_id:
-        return OrbiterBashOperator(task_id=task_id, bash_command=val.get("tes:command"))
+def basic_command_task(val: dict) -> OrbiterBashOperator | None:
+    """Translate `tes:command` into a `BashOperator`
+
+    ```pycon
+    >>> basic_command_task(val={'tes:name': 'foo', 'tes:command': 'echo "hi"'})
+    foo_task = BashOperator(task_id='foo', bash_command='echo "hi"')
+
+    ```
+    """
+    if command := val.get("tes:command"):
+        return OrbiterBashOperator(**task_common_args(val), bash_command=command)
+    return None
 
 
 @task_dependency_rule
-def basic_task_dependency_rule(val: OrbiterDAG) -> list | None:
-    """Translate input into a list of task dependencies"""
-    basedir = val.orbiter_kwargs["val"]["__file"].parent.resolve()
+def job_dependency_rule(val: OrbiterDAG) -> list | None:
+    """Use `Job.dependencies.xml` to find dependencies"""
     dependencies = []
+    __file: Path = val.orbiter_kwargs.get('val', {}).get("__file")
+    __input_dir: Path = val.orbiter_kwargs.get('val', {}).get("__input_dir")
+    basedir: Path = (__input_dir / __file.relative_to(__file.parts[0])).parent
     for path in basedir.rglob("*"):
         if path.name == "Job.dependencies.xml":
             with path.open() as f:
@@ -85,10 +115,10 @@ def basic_task_dependency_rule(val: OrbiterDAG) -> list | None:
 
 translation_ruleset = TranslationRuleset(
     file_type={FileTypeXML},
-    dag_filter_ruleset=DAGFilterRuleset(ruleset=[basic_dag_filter]),
-    dag_ruleset=DAGRuleset(ruleset=[basic_dag_rule]),
+    dag_filter_ruleset=DAGFilterRuleset(ruleset=[job_group_dag_filter]),
+    dag_ruleset=DAGRuleset(ruleset=[job_group_dag_rule]),
     task_filter_ruleset=TaskFilterRuleset(ruleset=[basic_task_filter]),
-    task_ruleset=TaskRuleset(ruleset=[basic_ssh_task]),
-    task_dependency_ruleset=TaskDependencyRuleset(ruleset=[basic_task_dependency_rule]),
+    task_ruleset=TaskRuleset(ruleset=[basic_command_task, create_cannot_map_rule_with_task_id_fn(lambda val: task_common_args(val)["task_id"])]),
+    task_dependency_ruleset=TaskDependencyRuleset(ruleset=[job_dependency_rule]),
     post_processing_ruleset=PostProcessingRuleset(ruleset=[]),
 )
