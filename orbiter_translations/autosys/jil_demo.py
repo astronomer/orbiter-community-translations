c487c060c74ea8b7a1a4fffa44faf808954d9709
fritz-astronomer <80706212+fritz-astronomer@users.noreply.github.com>
2025-11-05 20:34:57 -0500

    Autosys Demo Update (#48)
    * feat: update autosys demo
    
    * test: update test
    
    * fix: rm __init__.py to allow private translations to inherit community
    
    ---------
    
    Signed-off-by: fritz-astronomer <80706212+fritz-astronomer@users.noreply.github.com>

--- orbiter_translations/autosys/jil_demo.py
+++ orbiter_translations/autosys/jil_demo.py
@@ -3,29 +3,35 @@ r"""Demonstration of translating AutoSys JIL files into Airflow DAGs.
 Contact Astronomer @ https://astronomer.io/contact for access to our full translation.
 
 ```pycon
->>> translation_ruleset.test('''
+>>> res = translation_ruleset.test('''
 ... insert_job: foo.job
 ... job_type: CMD
 ... command: "C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" "arg3"
 ... machine: bar
 ... owner: foo@bar.com
 ... description: "Foo Job"
-... ''').dags["foo_job"]
+... insert_job: bar
+... job_type: CMD
+... command: echo "hello, world!"
+... machine: bar
+... '''); res.dags["foo_job"]; print('----'); res.dags["bar"]
 ... # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
 from airflow import DAG
-from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.operators.bash import BashOperator
 with DAG(dag_id='foo_job', default_args={'owner': 'foo@bar.com'}, doc_md=...):
-    foo_job_task = SSHOperator(task_id='foo_job', ssh_conn_id='bar', command='"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" "arg3"', doc='Foo Job')
+    foo_job_task = BashOperator(task_id='foo_job', bash_command='"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" "arg3"', doc='Foo Job')
+----
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+with DAG(dag_id='bar', doc_md=...):
+    bar_task = BashOperator(task_id='bar', bash_command='echo "hello, world!"')
 
 ```
 """
-
 from __future__ import annotations
 
-from orbiter_parsers.file_types.jil import FileTypeJIL
 from orbiter.objects.dag import OrbiterDAG
-from orbiter.objects.operators.ssh import OrbiterSSHOperator
-from orbiter.objects import conn_id
+from orbiter.objects.operators.bash import OrbiterBashOperator
 from orbiter.rules import (
     dag_filter_rule,
     dag_rule,
@@ -43,82 +49,42 @@ from orbiter.rules.rulesets import (
     translate,
     TaskDependencyRuleset,
 )
-
-
-def clean_to_airflow_valid_id(s: str) -> str:
-    """Replace invalid characters in a string to make it a valid Airflow ID."""
-    return s.replace(".", "_").lower()
-
-
-def task_common_args(val: dict) -> dict:
-    """
-    Generates common task arguments including task ID, documentation, and callbacks.
-
-    - `insert_job` -> `task_id`
-    - `description` -> `doc`
-
-    ```pycon
-    >>> task_common_args({'insert_job': 'foo.job', 'description': 'Foo Job'})
-    {'task_id': 'foo_job', 'doc': 'Foo Job'}
-
-    ```
-    :param val: A dictionary containing task-related information
-    :type val: dict
-    :return: Common task parameters
-    :rtype: dict
-    """
-    task_id = clean_to_airflow_valid_id(val.get("insert_job", "UNKNOWN"))
-    params = {"task_id": task_id}
-    if description := val.get("description"):
-        params["doc"] = description
-    return params
+from orbiter_parsers.file_types.jil import FileTypeJIL
 
 
 @dag_filter_rule
-def basic_filter_rule(val: dict) -> list[dict] | None:
-    r"""Basic filter rule to extract the `DAG` from an AutoSys dump.
-    ```pycon
-    >>> basic_filter_rule({
-    ...     'jobs': [
-    ...         {
-    ...             'insert_job': 'foo.job',
-    ...             'job_type': 'CMD',
-    ...             'machine': 'machine_id',
-    ...             'command': '"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd"'
-    ...         }
-    ...     ]
-    ... })
-    ... # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
-    [{'DAG':
-        [{'insert_job': 'foo.job',
-        'job_type': 'CMD',
-        'machine': 'machine_id',
-        'command': '"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd"'}]}]
+def demo_dag_filter_rule(val: dict) -> list[dict] | None:
+    """Pass parsed JIL through filter."""
+    return [val]
 
-    ```
-    """
-    return [{"DAG": val["jobs"]}]
+_demo_dag_common_args_params_doc = {
+    "owner": "DAG.default_args.owner",
+    "insert_job": "DAG.dag_id"
+}
+def demo_dag_common_args(val: dict) -> dict:
+    """Generates common DAG arguments."""
+    dag_id = val["insert_job"].replace(".", "_").lower()
+    params = {"dag_id": dag_id}
+    default_args = {}
+    if owners := val.get("owner"):
+        default_args["owner"] = owners
+    if default_args:
+        params["default_args"] = default_args
+    return params
 
-
-@dag_rule
-def basic_dag_rule(val: dict) -> OrbiterDAG | None:
+@dag_rule(params_doc=_demo_dag_common_args_params_doc)
+def demo_dag_rule(val: dict) -> OrbiterDAG | None:
     r"""Map AutoSysWorkflow into a DAG
 
-    - task `owner` -> DAG `owner`
-    - task `insert_job` ID -> DAG ID
-
     ```pycon
-    >>> basic_dag_rule({
+    >>> demo_dag_rule({
     ...   '__file': 'workflow.jil',
-    ...   'DAG': [{
-    ...     'command': '"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" '
-    ...     '"arg3"',
-    ...     'group': 'group_name',
-    ...     'insert_job': 'foo.job',
-    ...     'job_type': 'CMD',
-    ...     'machine': 'machine_id',
-    ...     'owner': 'foo@bar.net'
-    ...   }]
+    ...   'command': '"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" "arg3"',
+    ...   'group': 'group_name',
+    ...   'insert_job': 'foo.job',
+    ...   'job_type': 'CMD',
+    ...   'machine': 'machine_id',
+    ...   'owner': 'foo@bar.net'
     ... })
     ... # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
     from airflow import DAG
@@ -126,96 +92,74 @@ def basic_dag_rule(val: dict) -> OrbiterDAG | None:
 
     ```
     """
-    if dag := val.get("DAG"):
-        default_args = {}
-        owners = ",".join({owner for task in dag if (owner := task.get("owner"))})
-        if owners:
-            default_args["owner"] = owners
-        if default_args:
-            default_args = {"default_args": default_args}
-
-        dag_id = clean_to_airflow_valid_id(val["DAG"][0]["insert_job"])
+    if "insert_job" in val:
+        common_args = demo_dag_common_args(val)
         return OrbiterDAG(
-            dag_id=dag_id,
-            file_path=dag_id + ".py",
+            file_path=f"{common_args['dag_id']}.py",
             doc_md="**Created via [Orbiter](https://astronomer.github.io/orbiter) w/ Demo Translation Ruleset**.\n"
                    "Contact Astronomer @ [humans@astronomer.io](mailto:humans@astronomer.io) "
                    "or at [astronomer.io/contact](https://www.astronomer.io/contact/) for more!",
-            **default_args,
+            **common_args,
         )
+    return None
 
 
-@task_rule(priority=1)
-def ssh_command_rule(val) -> OrbiterSSHOperator | None:
-    r"""Maps a workflow of job_type 'CMD' into a SSHOperator
+@task_filter_rule
+def demo_task_filter(val: dict) -> list[dict] | None:
+    r"""Pass parsed JIL through filter."""
+    return [val]
 
-    - `command` becomes the command to run
-    - `machine` becomes the ssh_conn_id
+
+_demo_task_common_args_params_doc = {
+    "insert_job": "Operator.task_id",
+    "description": "Operator.doc"
+}
+def demo_task_common_args(val: dict) -> dict:
+    """Generates common task arguments.
 
     ```pycon
-    >>> ssh_command_rule({
+    >>> demo_task_common_args({'insert_job': 'foo.job', 'description': 'Foo Job'})
+    {'task_id': 'foo_job', 'doc': 'Foo Job'}
+
+    ```
+    """
+    task_id = val.get("insert_job", "UNKNOWN").replace(".", "_").lower()
+    params = {"task_id": task_id}
+    if description := val.get("description"):
+        params["doc"] = description
+    return params
+
+@task_rule(params_doc={"command": "BashOperator.bash_command"} | _demo_task_common_args_params_doc)
+def demo_bash_command_job_rule(val) -> OrbiterBashOperator | None:
+    r"""Maps a workflow of job_type 'CMD' into a BashOperator
+
+    ```pycon
+    >>> demo_bash_command_job_rule({
     ...     'insert_job': 'foo_bar',
     ...     'job_type': 'CMD',
     ...     'command': '"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" "arg3"',
-    ...     'machine': 'machine_id',
-    ...     'owner': 'foo@bar.net',
-    ...     'group': 'group_name',
     ... })
     ... # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
-    foo_bar_task = SSHOperator(task_id='foo_bar',
-    ssh_conn_id='machine_id',
-    command='"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" "arg3"')
+    foo_bar_task = BashOperator(task_id='foo_bar',
+        bash_command='"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" "arg3"')
 
     ```
     """
     if val.get("job_type") == "CMD":
-        return OrbiterSSHOperator(
-            command=val["command"],
-            **conn_id(conn_id=val["machine"], prefix="ssh", conn_type="ssh"),
-            **task_common_args(val),
+        return OrbiterBashOperator(
+            bash_command=val["command"],
+            **demo_task_common_args(val),
         )
-
-
-@task_filter_rule
-def basic_task_filter(val: dict) -> list[dict] | None:
-    r"""Returns the list of dictionaries that can be processed by the `@task_rules`
-    ```pycon
-    >>> basic_task_filter(
-    ... {
-    ...  'DAG': [{
-    ...  'command': '"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" '
-    ...  '"arg3"',
-    ...  'group': 'group_name',
-    ...  'insert_job': 'foo.job',
-    ...  'job_type': 'CMD',
-    ...  'machine': 'machine_id',
-    ... 'owner': 'foo@bar.net'}]})
-    ... # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
-    [{'command': '"C:\\ldhe\\cxl\\TidalDB\\startApp.cmd" "arg1" "arg2" "arg3"',
-    'group': 'group_name',
-    'insert_job': 'foo.job',
-    'job_type': 'CMD',
-    'machine': 'machine_id',
-    'owner': 'foo@bar.net'}]
-
-    ```
-    """
-    return val["DAG"]
+    return None
 
 
 translation_ruleset = TranslationRuleset(
     file_type={FileTypeJIL},
-    dag_filter_ruleset=DAGFilterRuleset(ruleset=[basic_filter_rule]),
-    dag_ruleset=DAGRuleset(ruleset=[basic_dag_rule]),
-    task_filter_ruleset=TaskFilterRuleset(ruleset=[basic_task_filter]),
-    task_ruleset=TaskRuleset(ruleset=[ssh_command_rule, create_cannot_map_rule_with_task_id_fn(lambda val: task_common_args(val)['task_id'])]),
+    dag_filter_ruleset=DAGFilterRuleset(ruleset=[demo_dag_filter_rule]),
+    dag_ruleset=DAGRuleset(ruleset=[demo_dag_rule]),
+    task_filter_ruleset=TaskFilterRuleset(ruleset=[demo_task_filter]),
+    task_ruleset=TaskRuleset(ruleset=[demo_bash_command_job_rule, create_cannot_map_rule_with_task_id_fn(lambda val: demo_task_common_args(val)['task_id'])]),
     task_dependency_ruleset=TaskDependencyRuleset(ruleset=[]),
     post_processing_ruleset=PostProcessingRuleset(ruleset=[]),
     translate_fn=translate,
 )
-
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
